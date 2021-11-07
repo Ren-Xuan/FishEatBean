db02dc0a0f2504cf1e95531002305d60405b45e2
RenXuan <40188916+Ren-Xuan@users.noreply.github.com>
2021-11-07 20:47:50 +0800

    Initial commit

---
+++ src/tools/FishUtilities.java
@@ -0,0 +1,38 @@
+/**
+  * @(#)tools.FishUtilities.java  2008-7-31  
+  * Copy Right Information	: Tarena
+  * Project					: tools
+  * JDK version used		: jdk1.6.4
+  * Comments				: 检测大嘴鱼和小鱼碰撞类。
+  * Version					: 1.0
+  * Sr	Date		Modified By		Why & What is modified
+  * 1.	2008-7-31 	小猪     		新建
+  **/
+package tools;
+
+import fish.BigMouthFish;
+import fish.FishBean;
+
+ /**
+ * 检测大嘴鱼和小鱼碰撞类。<br>
+ * 此类练习Math的静态函数的使用。
+ * 2008-7-31
+ * @author		达内科技[Tarena Training Group]
+ * @version	1.0
+ * @since		JDK1.6(建议) 
+ * @author		Administrator
+ */
+public class FishUtilities {
+
+	/**
+	 * 返回大嘴鱼是否吃掉了小鱼。<br>
+	 * 方式：检测2鱼(圆)之间是否相切或相交。<br>
+	 * 判断2圆心之间的距离小于2圆半径之和。此处判断小于大嘴鱼的半径即可。让大嘴鱼可以吃掉小鱼。
+	 * @param fish
+	 * @param bean
+	 * @return 返回大鱼是否吃掉小鱼。
+	 */
+	public static boolean isInteraction(BigMouthFish fish,FishBean bean){
+		return Math.pow(fish.posx+fish.size/2-bean.posx-bean.size/2,2)+Math.pow(fish.posy+fish.size/2-bean.posy-bean.size/2, 2)<=Math.pow(fish.size/2, 2);
+	}
+}
